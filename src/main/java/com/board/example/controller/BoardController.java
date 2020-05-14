879881e8ad180c783e37402ac2fd973a035e5958
ByeonGooHoon <dksekfldks65@naver.com>
2020-05-14 20:31:29 +0900

    게시판 업로드 작성중(1)

--- src/main/java/com/board/example/controller/BoardController.java
+++ src/main/java/com/board/example/controller/BoardController.java
@@ -4,20 +4,28 @@ import com.board.example.dto.BoardDto;
 import com.board.example.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
+@RequestMapping("/board")
 public class BoardController {
 
+    private final BoardService boardService;
+
     @Autowired
-    BoardService boardService;
+    public BoardController(BoardService boardService){
+        this.boardService = boardService;
+    }
 
     @GetMapping(value="/")
     public String index(){
         return "index";
     }
 
-    @GetMapping(value="board")
+    @GetMapping(value="/test")
     public String insertBoard(){
         BoardDto board = new BoardDto();
         board.setTitle("테스트");
@@ -29,6 +37,26 @@ public class BoardController {
         boardService.insertBoard(board);
 
         return "index";
+    }
+
+    @GetMapping(value="/")
+    public String goBoardPage(){
+
+        return "board";
+    }
+
+    @PostMapping(value="/")
+    public @ResponseBody Map<String, BoardDto> insertBoard(@RequestBody BoardDto boardDto){
+
+        Map<String, BoardDto> map = new HashMap<>();
+
+        try{
+
+        } catch(Exception e) {
+
+        }
+
+        return map;
     }
 
 }
