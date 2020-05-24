e23310deaa4c2f0e4d96810a0bb395089b273041
ByeonGooHoon <dksekfldks65@naver.com>
2020-05-24 23:06:06 +0900

    게시판 업데이트, 조회, 삽입 테스트 코드 작성

--- src/main/java/com/board/example/dto/BoardDto.java
+++ src/main/java/com/board/example/dto/BoardDto.java
@@ -1,10 +1,11 @@
 package com.board.example.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class BoardDto extends BaseDto{
 
     private Long id;
--- src/test/java/com/board/example/service/BoardServiceTest.java
+++ src/test/java/com/board/example/service/BoardServiceTest.java
@@ -1,12 +1,67 @@
 package com.board.example.service;
 
+import com.board.example.dto.BoardDto;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Transactional
 public class BoardServiceTest {
 
+    @Autowired
+    BoardService boardService;
+
+    //게시글 생성 테스트
+    @Test
+    public void insertBoardTest(){
+        BoardDto boardDto = new BoardDto();
+        boardDto.setTitle("제목");
+        boardDto.setWriter("구훈");
+        boardDto.setContent("내용");
+        boardDto.setRegpeId("hoon");
+        boardDto.setModpeId("hoon");
+        boardService.insertBoard(boardDto);
+    }
+
+    //게시글 조회 테스트
+    @Test
+    public void getBoardTest(){
+        BoardDto boardDto = new BoardDto();
+        boardDto.setTitle("제목");
+        boardDto.setWriter("구훈");
+        boardDto.setContent("내용");
+        boardDto.setRegpeId("hoon");
+        boardDto.setModpeId("hoon");
+        boardService.insertBoard(boardDto);
+        BoardDto savedBoardDto= boardService.getBoard(boardDto);
+        Assertions.assertEquals(boardDto.getTitle(), savedBoardDto.getTitle());
+        Assertions.assertEquals(boardDto.getContent(), savedBoardDto.getContent());
+        Assertions.assertEquals(boardDto.getWriter(), savedBoardDto.getWriter());
+    }
+
+    @Test
+    public void updateBoardTest(){
+        BoardDto boardDto = new BoardDto();
+        boardDto.setTitle("제목");
+        boardDto.setWriter("구훈");
+        boardDto.setContent("내용");
+        boardDto.setRegpeId("hoon");
+        boardDto.setModpeId("hoon");
+        boardService.insertBoard(boardDto);
+
+        boardDto.setTitle("제목 업데이트");
+        boardDto.setContent("내용 업데이트");
+        boardService.updateBoard(boardDto);
+
+        BoardDto updatedBoardDto = boardService.getBoard(boardDto);
+        Assertions.assertEquals(boardDto.getTitle(), updatedBoardDto.getTitle());
+        Assertions.assertEquals(boardDto.getContent(), updatedBoardDto.getContent());
+    }
 
 }
