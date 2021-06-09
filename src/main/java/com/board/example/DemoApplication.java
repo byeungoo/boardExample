d7c6241ed09c2cc9d7e8308cfbbe691f3687630d
byeungoo <dksekfldks65@naver.com>
2021-06-09 17:17:20 +0900

    dev rebase test3

--- src/main/java/com/board/example/DemoApplication.java
+++ src/main/java/com/board/example/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
     //main rebase test
     //main rebase test2
+    //main rebase test3
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
